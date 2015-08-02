7511ee9b3fd6764080ee362124674f3cc615ad04
Oscar Ferraro <oscar@oferraro.com>
2015-08-01 23:13:36 -0300

    First challenge, attach updated date to the "completado" on Click

--- clase1/js/app.js
+++ clase1/js/app.js
@@ -1,31 +1,41 @@
 var miApp = angular.module('misTareas',[]);
 
 miApp.controller('nombreCtrl',function($scope){
+    var fecha = new Date();
 	$scope.nombre = 'Ricardo';
 	$scope.listadoTareas = [
 		{
 			descripcion:'Instalar AngularJS',
-			completado:false
+			completado:false,
+            fecha: fecha
 		},
 		{
 			descripcion:'Bindear los datos',
-			completado:false
+			completado:false,
+            fecha: fecha
 		},
 		{
 			descripcion:'Agregar Tareas Automaticamente',
-			completado:false
+			completado:false,
+            fecha: fecha
 		}
 	]
-	$scope.fecha = new Date();
+	
 	$scope.agregarTarea = function(tarea){
 		var objTarea = {
 			descripcion:tarea,
-			completado:false
+			completado:false,
+            fecha: fecha
 		}
 		$scope.listadoTareas.push(objTarea);
 		$scope.nuevaTarea = '';
 
 	}
+    $scope.updateDate = function (tarea) {
+        if(tarea.completado) {
+            tarea.fecha = new Date();
+        }
+    }
 
 	$scope.contarIncompletos = function(){
 		var cantidadTareas = $scope.listadoTareas.length;
